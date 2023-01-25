a9cb4435e9ede018f4d83e4db96246f5e2a6311d
allesoares95 <allesoares_@live.com>
2023-01-25 11:49:56 -0300

    upd.button.class

---
+++ README.md
@@ -0,0 +1,14 @@
+# Iniciando projeto 
+
+`npm create vite@latest`
+
+## dependencia 
+
+`npm i `
+
+## Utilizando CSS styled-components
+
+`npm i styled-components`
+
+`npm i @types/styled-components -D`
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { ThemeProvider } from 'styled-components'
+import { Button } from "./components/Button";
 
-export function App() {}
+import { defaultTheme } from './styles/themes/default';
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+    </ThemeProvider>
+    )
+}
